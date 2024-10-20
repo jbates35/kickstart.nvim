b87430dc5940b70cefc1ea2528c06293cee9f9c2
Jimmy Bates <jbates.productions@gmail.com>
2024-10-19 21:53:55 -0700

    tsserver changed ts_ls

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -164,7 +164,7 @@ return { -- LSP Configuration & Plugins
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {
+        ts_ls = {
           init_options = {
             preferences = {
               disableSuggestions = true,
