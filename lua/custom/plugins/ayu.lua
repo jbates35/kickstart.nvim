94e04ff6911f667a79235fc6d1e98d5639b47fc9
Jimmy Bates <jbates.productions@gmail.com>
2026-05-26 14:10:57 -0700

    Coloring unuesd variable error differently so i can see them

--- lua/custom/plugins/ayu.lua
+++ lua/custom/plugins/ayu.lua
@@ -1,7 +1,10 @@
 vim.pack.add { { src = 'https://github.com/Shatur/neovim-ayu', name = 'ayu' } }
 
 vim.cmd.colorscheme 'ayu'
-
 require('ayu').setup {
   mirage = false,
 }
+
+-- Replace "#a0a0a0" with any hex color that fits your theme
+vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", { fg = "#88aaaa", underline = false })
+
